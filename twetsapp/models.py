fbf7f1edcedee34bb4e8244580b6a606ab77c55b
Vic <victormaina05@gmail.com>
2025-09-30 20:21:29 +0300

    update admin.py update models.py

--- twetsapp/models.py
+++ twetsapp/models.py
@@ -6,6 +6,9 @@ class Twet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=280)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:50]}"
